941ff2ef26ae16b201f1ab361a3c7aabbf5953c4
vihangarasanjali <rvihanga0506@gmail.com>
2025-08-21 21:36:17 +0530

    Built the Scooter API

--- src/main/java/com/codewithvihanga/store/entities/Scooter.java
+++ src/main/java/com/codewithvihanga/store/entities/Scooter.java
@@ -1,41 +1,27 @@
 package com.codewithvihanga.store.entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
+import lombok.*;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
-
-@Getter
-@Setter
 @Entity
-@Table(name = "scooters", schema = "schema")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "scooters")
 public class Scooter {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "model", nullable = false, length = 100)
     private String model;
 
-    @Lob
-    @Column(name = "description")
+    @Column(columnDefinition = "TEXT")
     private String description;
 
-    @Column(name = "price_per_hour", nullable = false)
     private Double pricePerHour;
 
-    @ColumnDefault("1")
-    @Column(name = "available")
-    private Boolean available;
+    private Boolean available = true;
 
-    @Column(name = "image_url")
     private String imageUrl;
-
-    @OneToMany(mappedBy = "scooter")
-    private Set<Booking> bookings = new LinkedHashSet<>();
-
 }
